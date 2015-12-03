346a260901520883b879353ec539f3625434e442
Praxis <praxis@vada.local>
2015-12-03 12:25:45 -0600

    Initial Commit

---
+++ pruebaSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaSwift
+//
+//  Created by Praxis on 03/12/15.
+//  Copyright © 2015 Praxis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
